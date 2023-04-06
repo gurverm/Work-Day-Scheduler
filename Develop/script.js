2ebd3f666051004f0974347eff8bad203eb1ecfc
Gurveer Madurai <maduraigurveer@gmail.com>
2023-04-05 18:25:13 -0700

    code applied to past present and future boxes in js

--- Develop/script.js
+++ Develop/script.js
@@ -2,6 +2,9 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+  var today = dayjs();
+
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -9,7 +12,7 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
-  var timeBlockEl = $('.time-block');
+  var timeBlockBoxesEl = $('.time-block');
 
   $('.saveBtn').on('click', function(){
     var timeBlockEl = $(this).closest('.time-block'); // 'this' refers to any DOM element that triggered its event. In this case it is the saveBtn or button.
@@ -20,14 +23,6 @@ $(function () {
   });
 
 
-
-
-
-
-
-
-
-
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
@@ -35,13 +30,20 @@ $(function () {
   // current hour in 24-hour time?
   //
 
+  timeBlockBoxesEl.each(function(){
 
+    var timeBlockEl = $(this);
+    var timeBlockElId = timeBlockEl.attr("id");
+    var hour = dayjs(timeBlockElId, "hA");
 
-
-
-
-
-
+    if (hour.isBefore(today, "hour")) {
+      timeBlockEl.addClass("past");
+    } else if (hour.isSame(today, "hour")) {
+      timeBlockEl.addClass("present");
+    } else {
+      timeBlockEl.addClass("future");
+    }
+  });
 
 
   // TODO: Add code to get any user input that was saved in localStorage and set
@@ -49,33 +51,21 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
 
-  timeBlockEl.each(function () {
+  timeBlockBoxesEl.each(function () {
 
     var timeBlockEl = $(this); // 'this' will refer to the time block class but respond to this function
     var timeBlockElId = timeBlockEl.attr("id");
 
     var description = localStorage.getItem(timeBlockElId);
-    
+
     if (description) {
       timeBlockEl.find(".description").val(description);
     }
   });
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-  // TODO: Add code to display the current date in the header of the page.
-  var today = dayjs();
+    // TODO: Add code to display the current date in the header of the page.
+  
   var currentDate = $('#currentDay');
   currentDate.text(today.format("dddd MMMM D, YYYY. h:mm:ss:a"));
 });
